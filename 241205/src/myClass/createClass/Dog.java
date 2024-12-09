063a194acb2a18cfb7bdcfd4f044e2d26e2aa95e
Hyojin611 <611gywls@gmail.com>
2024-12-09 17:40:00 +0900

    Delete 241205 directory

--- 241205/src/myClass/createClass/Dog.java
+++
@@ -1,9 +0,0 @@
-package myClass.createClass;
-
-public class Dog {
-   public final String type = "말티즈"; // 바뀌지 않는 것은 final로 앞에 선언
-    public String name = "토토";
-    public int age = 5;
-    public String color = "하얀색";
-
-}
